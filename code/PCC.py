9ece451482804aca65e3b56c2af8499e94771cbf
sergeyvolodin <sergeyvolodin@bcded152-e50b-4b38-bcdb-0829fdd364ce>
2016-08-21 17:10:41 +0000

    Add fast hamming and subset loss prediction
    git-svn-id: svn://svn.code.sf.net/p/mlalgorithms/code/Group374/Volodin2016ProbabilisticReceptorPrediction@12230 bcded152-e50b-4b38-bcdb-0829fdd364ce

--- code/PCC.py
+++ code/PCC.py
@@ -4,15 +4,31 @@ from sklearn.linear_model import LogisticRegression as LR
 from sklearn.metrics import hamming_loss
 from copy import deepcopy
 class PCC:
+    # saved dataset array
+    # item = dataset for i'th classifier
     Xc = None
     Yc = None
+    
+    # classifiers array
     C = None
+    
+    # saved dataset
     X = None
     Y = None
+    
+    # missing value in answers
     badValue = None
+    
+    # number of labels
     m = 0
+    
+    # MLC loss function
     loss_ = None
+    
+    # one-dimensional estimator
     estimator = None
+    
+    # constructor
     def __init__(self, badValue = 999, loss = hamming_loss, estimator = None):
         self.badValue = badValue
         self.loss = loss
@@ -20,6 +36,8 @@ class PCC:
         if estimator == None:
             estimator = LR()
         return None
+    
+    # init arrays
     def initialize(self, X, Y):
         self.X = np.copy(X)
         self.Y = np.copy(Y)
@@ -27,10 +45,15 @@ class PCC:
         self.Xc = [None] * self.m
         self.Yc = [None] * self.m
         self.C = [None] * self.m
+        
+    # fitOne for all labels
     def fit(self, X, Y):
         self.initialize(X, Y)
         for i in range(Y.shape[1]):
             self.fitOne(i)
+            
+    # add answers as features
+    # run estimator.fit()
     def fitOne(self, i):
         self.C[i] = None
         ind = np.where(self.Y[:, i] != self.badValue)
@@ -39,30 +62,18 @@ class PCC:
         Y1 = np.copy(self.Y[ind][:, 0:i])
         
         if Y1.shape[1] > 0:
-            Xc = np.concatenate((self.X[ind], Y1), axis=1)
+            Xc = np.concatenate((self.X[ind], Y1), axis = 1)
         else:
             Xc = self.X[ind]
         
         self.Xc[i] = Xc
         self.Yc[i] = Yc
         
-        #class_weight = 'balanced'
-        # fit_intercept = False
         tmpLR = deepcopy(self.estimator)
         tmpLR.fit(Xc, Yc)
         self.C[i] = tmpLR
-        self.showFPRTPR(i)
-        
-        print("i=%d coef=%s" % (i, str(tmpLR.coef_)))
-    def showFPRTPR(self, i):
-        Xc = self.Xc[i]
-        Yc = self.Yc[i]
-        classifier = self.C[i]
-        N = len(np.where(Yc == 0)[0])
-        P = len(np.where(Yc == 1)[0])
-        TP = len(np.where((classifier.predict(Xc) == 1) & (Yc == 1))[0])
-        FP = len(np.where((classifier.predict(Xc) == 1) & (Yc == 0))[0])
-       # print("Got FPR=%.1f %% TPR=%.1f %%" % (100. * FP / N, 100. * TP / P))
+    
+    # time: O(2^m)
     def probabilityItem(self, x, y):
         res = 1
         for i in range(len(y)):
@@ -70,6 +81,8 @@ class PCC:
             if self.C[i] != None:
                 res *= self.C[i].predict_proba([x1])[0][y[i]]
         return(res)
+    
+    # time: O(2^{2m})
     def predictObject(self, x):
         smin = None
         res = None
@@ -81,6 +94,11 @@ class PCC:
                 smin = s
                 res = v
         return(res)
+    
+    # time: O(2^{2m} * |X|)
+    # warning: this method uses Bayes optimal decisions
+    # for arbitrary loss, which is slow
+    # use predict<Metric> for faster prediction
     def predict(self, X):
         X = np.array(X)
         if len(X.shape) == 1:
@@ -92,5 +110,53 @@ class PCC:
             return(res)
         else:
             print("Wrong X shape " + str(X.shape))
+            
+    # time: O(2^m)
+    def predictMarginal(self, x, i, v0 = []):
+        # reached required label
+        x1 = np.concatenate((np.array(x), np.array(v0)))
+        if len(v0) == i:
+            return self.C[i].predict_proba([x1])[0][1]
+        elif len(v0) < i:
+            j = len(v0)
+            res = 0
+            for v1 in [0, 1]:
+                # new vector
+                v01 = list(np.copy(v0))
+                v01.append(v1)
+                p = self.C[j].predict_proba([x1])[0][v1]
+                res += p * self.predictMarginal(x, i, v01)
+            return res
+        
+    # time: O(2^m)
+    # 1 + 2 + ... + 2^m
+    def predictMarginals(self, x):
+        res = [0] * self.m
+        for i in range(self.m):
+            res[i] = self.predictMarginal(x, i)
+        return(np.array(res))
+    
+    # time: O(2^m)
+    def predictSubsetLoss(self, x):
+        # mode of the distribution
+        vmax = None
+        pmax = 0
+        for v in getB(self.m):
+            p = self.probabilityItem(x, v)
+            if p > pmax:
+                pmax = p
+                vmax = v
+        return vmax
+    
+    # time: O(2^m)
+    def predictHammingLoss(self, x):
+        return self.predictMarginals(x) >= 0.5
+    
+    # string representation
     def __repr__(self):
         return("PCC X %d x %d Y %d x %d m %d loss = %s" % (self.X.shape[0], self.X.shape[1], self.Y.shape[0], self.Y.shape[1], self.m, self.loss))
+    
+    # print P(y|x) for given x and all possible y
+    def printDistribution(self, x):
+        for v in getB(self.m):
+            print "v = " + str(v) + " p = " + str(self.probabilityItem(x, v))
